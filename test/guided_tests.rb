31f6f05307cadc7a27c1986c15bddbd2c35558dc
Gabrielle Epstein <doctorgabi@gmail.com>
2013-12-19 18:58:35 -0600

    Changes all month references to day.

--- test/guided_tests.rb
+++ test/guided_tests.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH << "test"
 $LOAD_PATH << "lib"
 #load path is how ruby knows where to look for things. Add these directories to the places you're looking for files.
-require 'test_month'
+require 'test_day'
 require 'test_cal'

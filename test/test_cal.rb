343938ed1f591a9191d427cb52b55d87d598d42a
Gabrielle Epstein <doctorgabi@gmail.com>
2013-12-20 10:08:01 -0600

    Changes to Minitest.

--- test/test_cal.rb
+++ test/test_cal.rb
@@ -1,20 +1,25 @@
-require 'test/unit'
+require "minitest/autorun"
 require "cal"
+
 # require_relative 'helper'
 
-class TestCalendar < Test::Unit::TestCase
+class TestCalendar < MiniTest::Unit::TestCase
 
   def test_month_day_year_output_properly
-  #   ruby takes_arguments.rb 02 2012
-  # end
-  # shell_output = ruby cal.rb 02 2012
     shell_output = `ruby lib/cal.rb 02 2012`
     expected_output = "month 02, year 2012, day Wednesday"
     assert_equal(expected_output, shell_output)
   end
 
-  # def test_cal_integration
-  #   assert_equal(`cal 2 2012`, `ruby cal.rb 2 2012`)
-  # end
+  def test_cal_integration
+    assert_equal("
+   February 2012
+Su Mo Tu We Th Fr Sa
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29         ",`ruby lib/cal.rb 2 2012`)
+  end
 
 end

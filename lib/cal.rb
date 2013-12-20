afd98b71029da07d8604974abd2092a7a896253d
Gabrielle Epstein <doctorgabi@gmail.com>
2013-12-19 20:27:39 -0600

    Starting work on integration tests.

--- lib/cal.rb
+++ lib/cal.rb
@@ -3,4 +3,8 @@ require "day"
 
 month = ARGV[0]
 year = ARGV[1]
-puts "month #{month}, year #{year}"
+
+weekday = Day.new
+weekday.zeller(month, year) #to get the day of the week to start on.
+
+puts "month #{month}, year #{year}, day #{weekday}"
--- test/test_cal.rb
+++ test/test_cal.rb
@@ -4,9 +4,16 @@ require "cal"
 
 class TestCalendar < Test::Unit::TestCase
 
-  # def test_
-
+  def test_month_day_year_output_properly
+  #   ruby takes_arguments.rb 02 2012
   # end
+  shell_output = "ruby cal.rb 02 2012"
+      expected_output = <<EOS
+"month 2, year 2012, day Wednesday"
+EOS
+    assert_equal shell_output, expected_output
+  end
+
   # def test_cal_integration
   #   assert_equal(`cal 2 2012`, `ruby cal.rb 2 2012`)
   # end

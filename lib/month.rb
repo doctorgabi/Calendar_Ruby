56bc4c32bd183143817fb529136f2619fc93ca5b
Gabrielle Epstein <doctorgabi@gmail.com>
2013-12-19 18:46:32 -0600

    Correct output for test 2.

--- lib/month.rb
+++ lib/month.rb
@@ -1,40 +1,44 @@
-#this is a regular class file, where the code will go
+
 class Month
 
   def zeller(*values)
-    m = 1
-    y = 1904
+    if values.length == 0
+      "Please input a month and a year in numbers"
+    else
+      m = values[0].to_i
+      y = values[1].to_i
+      # "#{m}, #{y}"
+      y -= 1 if m == 1 || 2
+      m += 12 if m == 1 || 2
 
-    y -= 1 if m == 1 || 2
-    m += 12 if m = 1 || 2
+      q = 1
+      k = y % 100
+      j = (y / 100).floor
 
-    q = 1
-    K = y % 100
-    J = (y / 100).floor
+      a = (( 13 * (m + 1) ) / 5).floor
+      b = ( k / 4 ).floor
+      c = ( j / 4 ).floor
 
-    a = (( 13 * (m + 1) ) / 5).floor
-    b = ( K / 4 ).floor
-    c = ( J / 4 ).floor
+      zellers_day_value = ( q + a + k + b + c + (5 * j)) % 7
 
+      if zellers_day_value == 0
+        @weekday = "Saturday"
+      elsif zellers_day_value == 1
+        @weekday = "Sunday"
+      elsif zellers_day_value == 2
+        @weekday = "Monday"
+      elsif zellers_day_value == 3
+        @weekday = "Tuesday"
+      elsif zellers_day_value == 4
+        @weekday = "Wednesday"
+      elsif zellers_day_value == 5
+        @weekday = "Thursday"
+      elsif zellers_day_value == 6
+        @weekday = "Friday"
+      end
 
-    zellers_day_value = ( q + a + K + b + c + (5 * J)) % 7
-
-    if zellers_day_value == 0
-      @weekday = "Saturday"
-    elsif zellers_day_value == 1
-      @weekday = "Sunday"
-    elsif zellers_day_value == 2
-      @weekday = "Monday"
-    elsif zellers_day_value == 3
-      @weekday = "Tuesday"
-    elsif zellers_day_value == 4
-      @weekday = "Wednesday"
-    elsif zellers_day_value == 5
-      @weekday = "Thursday"
-    elsif zellers_day_value == 6
-      @weekday = "Friday"
+      @weekday
     end
-    puts @weekday
 
   end
 
--- test/test_month.rb
+++ test/test_month.rb
@@ -7,5 +7,32 @@ class MonthTest < Test::Unit::TestCase
     month = Month.new.zeller
   end
 
+  def test_02_leap_year_02_2012
+    month = Month.new
+    day = month.zeller(2, 2012)
+    assert_equal("Wednesday", day)
+  end
+
+  def test_03_starts_on_Sat_01_2012
+  end
+
+  def test_04_not_a_leap_year_02_1900
+  end
+
+  def test_05_leap_year_div_by_100_and_400
+  end
+
+  def test_06_outside_of_scope_05_3005
+  end
+
+  def test_07_no_arguments
+  end
+
+  def test_08_four_rows_month_02_2015
+  end
+
+  def test_09_six_rows_month_09_2012
+  end
+
 
 end

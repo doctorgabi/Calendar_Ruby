b5c12a5f734e74528ef8f92f3d40e2b905171b68
Gabrielle Epstein <doctorgabi@gmail.com>
2014-01-07 14:22:26 -0600

    Alters integration tests from lib/cal.rb to cal.rb and adds some integration cases.

--- test/test_cal.rb
+++ test/test_cal.rb
@@ -23,53 +23,68 @@ class TestCalendar < MiniTest::Unit::TestCase
     assert_equal(`ruby lib/cal.rb 04 2004 11`, "Please enter only a month and/or a year.")
   end
 
-  # def test_06_no_arguments_returns_the_current_month_and_year ##`cal` is outputting wierdly.
-  #   assert_equal(`cal`,`ruby lib/cal.rb`)
-  # end
-
-  def test_07_month_and_year_but_year_not_four_digits_fails
+  def test_06_month_and_year_but_year_not_four_digits_fails
     assert_equal(`ruby lib/cal.rb 09 03`, "cal: year `03' not in range 1800..3000")
   end
 
-
-  ##-----------------------------------------------------------------------------------------
-  ##            All subsequent tests appear to output perfectly yet fail.
-  ##-----------------------------------------------------------------------------------------
-
-  def test_08_month_with_1st_on_Sunday_works
-    assert_equal(`cal 01 2012`, `ruby lib/cal.rb 01 2012`)
+  def test_07_no_arguments_returns_the_current_month_and_year ##`cal` is outputting wierdly, adding "\b \b"
+    assert_equal(`cal`,`ruby cal.rb`)
   end
 
-  def test_09_month_with_1st_on_Monday_works
-    assert_equal(`cal 08 2011`, `ruby lib/cal.rb 08 2011`)
+  #-----------------------------------------------------------------------------------------
+  #            Integration tests: All appear to output perfectly yet fail.
+  #-----------------------------------------------------------------------------------------
+
+  def test_08_cal_integration_month_with_1st_on_Sunday_works
+    assert_equal(`cal 01 2012`, `ruby cal.rb 01 2012`)
   end
 
-  def test_10_month_with_1st_on_Tuesday_works
-    assert_equal(`cal 3 2011`, `ruby lib/cal.rb 3 2011`)
+  def test_09_cal_integration_month_with_1st_on_Monday_works
+    assert_equal(`cal 08 2011`, `ruby cal.rb 08 2011`)
   end
 
-  def test_11_month_with_1st_on_Wednesday_works
-    assert_equal(`cal 4 2009`, `ruby lib/cal.rb 4 2009`)
+  def test_10_cal_integration_month_with_1st_on_Tuesday_works
+    assert_equal(`cal 3 2011`, `ruby cal.rb 3 2011`)
   end
 
-  def test_12_month_with_1st_on_Thursday_works
-    assert_equal(`cal 1 2009`, `ruby lib/cal.rb 1 2009`)
+  def test_11_cal_integration_month_with_1st_on_Wednesday_works
+    assert_equal(`cal 4 2009`, `ruby cal.rb 4 2009`)
   end
 
-  def test_13_month_with_1st_on_Friday_works
-    assert_equal(`cal 5 2009`, `ruby lib/cal.rb 5 2009`)
+  def test_12_cal_integration_month_with_1st_on_Thursday_works
+    assert_equal(`cal 1 2009`, `ruby cal.rb 1 2009`)
   end
 
-  def test_14_month_with_1st_on_Saturday_works
-    assert_equal(`cal 12 2007`, `ruby lib/cal.rb 12 2007`)
+  def test_13_cal_integration_month_with_1st_on_Friday_works
+    assert_equal(`cal 5 2009`, `ruby cal.rb 5 2009`)
   end
 
-  def test_15_year_only_non_leap
-    assert_equal(`cal 1983`, `ruby lib/cal.rb 1983`)
+  def test_14_cal_integration_month_with_1st_on_Saturday_works
+    assert_equal(`cal 12 2007`, `ruby cal.rb 12 2007`)
+  end
+
+  def test_15_cal_integration_year_only_non_leap
+    assert_equal(`cal 1983`, `ruby cal.rb 1983`)
   end
 
   def test_16_cal_integration
-    assert_equal(`cal 02 2012`, `ruby lib/cal.rb 2 2012`)
+    assert_equal(`cal 02 2012`, `ruby cal.rb 2 2012`)
+  end
+
+  def test_16_cal_integration_weird_non_leap_year
+    assert_equal(`cal 1900`, `ruby cal.rb 1900`)
+  end
+
+  def test_17_cal_integration_normal_non_leap_year
+    assert_equal(`cal 1999`, `ruby cal.rb 1999`)
+  end
+
+  def test_18_cal_integration_normal_leap_year_divisible_by_400
+    assert_equal(`cal 2000`, `ruby cal.rb 2000`)
+  end
+
+  def test_19_cal_integration_normal_leapyear_after_weird_non_leap_year
+    assert_equal(`cal 1904`, `ruby cal.rb 1904`)
   end
 
 end
